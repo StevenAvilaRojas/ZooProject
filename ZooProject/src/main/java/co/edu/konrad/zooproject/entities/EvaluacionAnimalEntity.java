67b81815b0013beb370f21639e29622c0358881b
Diego Cortazar <cortazardiego@gmail.com>
2019-02-26 17:16:30 -0500

    Created "EvaluacionAnimalEntity" class and "UbicacionEntity" class.

---
+++ ZooProject/src/main/java/co/edu/konrad/zooproject/entities/EvaluacionAnimalEntity.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.markedplace.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Diego
+ */
+
+@Entity
+public class EvaluacionAnimalEntity implements Serializable{
+
+    //Llave Primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_evaluacion;
+
+    //Id Animal
+    @Column(name = "id_animal")
+    private Long idAnimal;
+
+    //Observacion
+    @Column
+    private String observacion;
+
+    //Calificacion
+    @Column
+    private Long calificacion;
+
+    //Constructor
+
+    public EvaluacionAnimalEntity() {
+    }
+
+
+    //Getters and Setters
+
+    public Long getId_evaluacion() {
+        return id_evaluacion;
+    }
+
+    public void setId_evaluacion(Long id_evaluacion) {
+        this.id_evaluacion = id_evaluacion;
+    }
+
+    public Long getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(Long idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public Long getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Long calificacion) {
+        this.calificacion = calificacion;
+    }
+
+
+
+}
---
+++ ZooProject/src/main/java/co/edu/konrad/zooproject/entities/UbicacionEntity.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.zooproject.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Diego
+ */
+
+@Entity
+public class UbicacionEntity implements Serializable{
+
+    //Llave Primaria
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id_ubicacion;
+
+
+   //Ocupado
+   @Column
+   private boolean ocupado;
+
+
+   //Constructor
+    public UbicacionEntity() {
+    }
+
+    //Getter And Setter
+
+    public Long getId_ubicacion() {
+        return id_ubicacion;
+    }
+
+    public void setId_ubicacion(Long id_ubicacion) {
+        this.id_ubicacion = id_ubicacion;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+}
