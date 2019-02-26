ed49ce16830cb5fb8f2e3580d792a9399ffb4c2f
Steven Avila Rojas <stevenleo08@gmail.com>
2019-02-26 16:28:53 -0500

    Updated "AnimalEntity" object.

--- ZooProject/src/main/java/co/edu/konrad/zooproject/entities/AnimalEntity.java
+++ ZooProject/src/main/java/co/edu/konrad/zooproject/entities/AnimalEntity.java
@@ -5,14 +5,80 @@
  */
 package co.edu.konrad.zooproject.entities;
 
+import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
- * @author Ingenieria
+ * @author Steven
  */
 @Entity
-public class AnimalEntity  implements serializable{
-    
-    
+public class AnimalEntity  implements Serializable{
+    //Llave Primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idAnimal;
+    //Nombre Animal
+    @Column(name ="nombre_animal")
+    private String nombreAnimal;
+    //Nombre Cientifico
+    @Column(name ="nom_cientifico")
+    private String nombreCientifico;
+    //Especie
+    @Column
+    private String especie;
+    //Descripcion
+    @Column
+    private String descripcion;
+    //Constructor getters and setters
+    public AnimalEntity() {
+       }
+
+       public Long getIdAnimal() {
+           return idAnimal;
+       }
+
+       public void setIdAnimal(Long idAnimal) {
+           this.idAnimal = idAnimal;
+       }
+
+       public String getNombreAnimal() {
+           return nombreAnimal;
+       }
+
+       public void setNombreAnimal(String nombreAnimal) {
+           this.nombreAnimal = nombreAnimal;
+       }
+
+       public String getNombreCientifico() {
+           return nombreCientifico;
+       }
+
+       public void setNombreCientifico(String nombreCientifico) {
+           this.nombreCientifico = nombreCientifico;
+       }
+
+       public String getEspecie() {
+           return especie;
+       }
+
+       public void setEspecie(String especie) {
+           this.especie = especie;
+       }
+
+       public String getDescripcion() {
+           return descripcion;
+       }
+
+       public void setDescripcion(String descripcion) {
+           this.descripcion = descripcion;
+       }
+
+
+
+
 }
