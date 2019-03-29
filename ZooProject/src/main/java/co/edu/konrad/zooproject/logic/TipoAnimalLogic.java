53dfc967661b3da1ef4854d39a5dc9372c2e3e2e
Diego Cortazar <cortazardiego@gmail.com>
2019-03-28 19:03:26 -0500

    Created "TipoAnimalLogic" and "UbicacionLogic"

---
+++ ZooProject/src/main/java/co/edu/konrad/zooproject/logic/TipoAnimalLogic.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.zooproject.logic;
+
+import co.edu.konrad.zooproject.entities.TipoAnimalEntity;
+import co.edu.konrad.zooproject.persistence.TipoAnimalPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Diego Cortázar, Steven Avila, Alexander Nicholls
+ */
+@Stateless
+public class TipoAnimalLogic {
+
+    @Inject
+    private TipoAnimalPersistence persistence;
+
+    public List<TipoAnimalEntity> obtenerTipoAnimal() {
+        List<TipoAnimalEntity> tipoAnimal = persistence.findAll();
+        return tipoAnimal;
+    }
+
+    /**
+     * obtener TipoAnimal
+     *
+     * @return Id
+     */
+    public TipoAnimalEntity obtenerTipoAnimal(Long id) throws IllegalAccessException {
+        TipoAnimalEntity tipoAnimal = persistence.find(id);
+        if (tipoAnimal == null) {
+            throw new IllegalAccessException("No Se Tipo Animal");
+        }
+        return tipoAnimal;
+    }
+
+    /**
+     * obtener TipoAnimal por Id
+     *
+     * @return Tipo de Animal
+     */
+    public TipoAnimalEntity obtenerTipoAnimalPorId(Long id) throws IllegalAccessException {
+        TipoAnimalEntity tipoAnimal = persistence.find(id);
+        if (tipoAnimal == null) {
+            throw new IllegalAccessException("No Se Encontro Tipo de Animal");
+        }
+        return tipoAnimal;
+    }
+
+    /**
+     * @param tipoAnimal crear tipoAnimal
+     * @return tipo de animal
+     */
+    public TipoAnimalEntity crearTipoAnimal(TipoAnimalEntity tipoAnimal) {
+        persistence.create(tipoAnimal);
+        return tipoAnimal;
+    }
+
+    /**
+     * @param Id, tipoAnimal actualizar tipoAnimal
+     * @param tipoAnimal
+     * @return tipoAnimalActualizar
+     */
+    public TipoAnimalEntity actualizarTipoAnimal(Long Id, TipoAnimalEntity tipoAnimal) {
+        TipoAnimalEntity tipoAnimalActualizar = persistence.update(tipoAnimal);
+        return tipoAnimalActualizar;
+
+    }
+
+    /**
+     * eliminar tipoAnimal
+     *
+     * @param Id
+     *
+     */
+    public void eliminarTipoAnimal(Long Id) {
+        persistence.delete(Id);
+
+    }
+
+}
---
+++ ZooProject/src/main/java/co/edu/konrad/zooproject/logic/UbicacionLogic.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.konrad.zooproject.logic;
+
+import co.edu.konrad.zooproject.entities.UbicacionEntity;
+import co.edu.konrad.zooproject.persistence.UbicacionPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Diego Cortázar, Steven Avila, Alexander Nicholls
+ */
+@Stateless
+public class UbicacionLogic {
+
+    @Inject
+    private UbicacionPersistence persistence;
+
+    public List<UbicacionEntity> obtenerUbicacion() {
+        List<UbicacionEntity> ubicacion = persistence.findAll();
+        return ubicacion;
+    }
+
+    /**
+     * obtener Ubicacion
+     *
+     * @return Id
+     */
+    public UbicacionEntity obtenerUbicacion(Long id) throws IllegalAccessException {
+        UbicacionEntity ubicacion = persistence.find(id);
+        if (ubicacion == null) {
+            throw new IllegalAccessException("No Se Encontro Ubicacion");
+        }
+        return ubicacion;
+    }
+
+    /**
+     * obtener Ubicacion por Id
+     *
+     * @return Ubicacion
+     */
+    public UbicacionEntity obtenerUbicacionPorId(Long id) throws IllegalAccessException {
+        UbicacionEntity ubicacion = persistence.find(id);
+        if (ubicacion == null) {
+            throw new IllegalAccessException("No Se Encontro Ubicacion");
+        }
+        return ubicacion;
+    }
+
+    /**
+     * @param ubicacion crear ubicacion
+     * @return ubicacion
+     */
+    public UbicacionEntity crearUbicacion(UbicacionEntity ubicacion) {
+        persistence.create(ubicacion);
+        return ubicacion;
+    }
+
+    /**
+     * @param Id, ubicacion actualizar ubicacion
+     * @param ubicacion
+     * @return ubicacionActualizar
+     */
+    public UbicacionEntity actualizarUbicacion(Long Id, UbicacionEntity ubicacion) {
+        UbicacionEntity ubicacionActualizar = persistence.update(ubicacion);
+        return ubicacionActualizar;
+
+    }
+
+    /**
+     * eliminar ubicacion
+     *
+     * @param Id
+     *
+     */
+    public void eliminarUbicacion(Long Id) {
+        persistence.delete(Id);
+
+    }
+
+}
