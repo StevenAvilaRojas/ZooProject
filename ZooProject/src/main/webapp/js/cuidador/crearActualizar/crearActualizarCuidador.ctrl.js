512f9d07e98f756fec6592968e23efdbcc3cac91
Diego Cortazar <cortazardiego@gmail.com>
2019-05-17 08:17:35 -0500

    Updated CrearActualizar.js

--- ZooProject/src/main/webapp/js/cuidador/crearActualizar/crearActualizarCuidador.ctrl.js
+++ ZooProject/src/main/webapp/js/cuidador/crearActualizar/crearActualizarCuidador.ctrl.js
@@ -1,7 +1,41 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var productoModule = angular.module('cuidadorModule');
+
+productoModule.controller('caCuidadorCtrl', ['$scope', '$http', '$state', function($scope, $http, $state){
+      var id = $state.params.id;
+      
+      $scope.cuidador = {};
+      
+      if(id){
+        $http.get('api/cuidadores/'+id).then(function(response){
+            $scope.cuidador = response.data;
+        }, function(error){
+            console.log(error);
+        });
+      }
+            
+      $scope.guardarCuidador = function(){
+        if($scope.cuidador.nomCompleto && $scope.cuidador.numeroId && $scope.cuidador.tipoId && $scope.cuidador.fechaNacimiento && $scope.cuidador.celular ){
+            $http.post('api/cuidadores', JSON.stringify($scope.cuidador)).then(function(response){
+                $scope.cuidador = {};
+                //Redireccion a la lista
+                $state.go('cuidadores');
+            }, function(error){
+                console.log(error);
+            });
+        }
+      };
+      
+      $scope.actualizarCuidador = function(){
+         if($scope.cuidador.nomCompleto && $scope.cuidador.numeroId && $scope.cuidador.tipoId && $scope.cuidador.fechaNacimiento && $scope.cuidador.celular ){
+             $http.put('api/cuidadores/'+id, JSON.stringify($scope.cuidador)).then(function(response){
+                 $scope.cuidador = {};
+                 $state.go('cuidadores');
+             }, function(error){
+                console.log(error); 
+             });
+             
+         } 
+      };
+}]);
 
 
