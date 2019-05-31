79c26c006516e46aaabb4b01e0d0a7df84b19794
lexnicholls <nicholls.aom@gmail.com>
2019-05-31 07:53:41 -0500

    Updated Ubicacion

---
+++ ZooProject/src/main/webapp/js/ubicacion/crearActualizar/crearActualizarUbicacion.ctrl.js
@@ -0,0 +1,41 @@
+var ubicacionModule = angular.module('ubicacionModule');
+
+ubicacionModule.controller('caUbicacionCtrl', ['$scope', '$http', '$state', function($scope, $http, $state){
+      var id = $state.params.id;
+      
+      $scope.ubicacion = {};
+      
+      if(id){
+        $http.get('api/ubicaciones/'+id).then(function(response){
+            $scope.ubicacion = response.data;
+        }, function(error){
+            console.log(error);
+        });
+      }
+            
+      $scope.guardarUbicacion = function(){
+        if($scope.ubicacion.id_ubicacion && $scope.ubicacion.ocupado ){
+            $http.post('api/ubicaciones', JSON.stringify($scope.ubicacion)).then(function(response){
+                $scope.ubicacion = {};
+                //Redireccion a la lista
+                $state.go('ubicaciones');
+            }, function(error){
+                console.log(error);
+            });
+        }
+      };
+      
+      $scope.actualizarCuidador = function(){
+         if($scope.ubicacion.id_ubicacion  && $scope.ubicacion.ocupado ){
+             $http.put('api/ubicaciones/'+id, JSON.stringify($scope.ubicacion)).then(function(response){
+                 $scope.ubicacion = {};
+                 $state.go('ubicaciones');
+             }, function(error){
+                console.log(error); 
+             });
+             
+         } 
+      };
+}]);
+
+
---
+++ ZooProject/src/main/webapp/js/ubicacion/ubicacion.module.js
@@ -0,0 +1,1 @@
+var ubicacionModule = angular.module("ubicacionModule", []);
